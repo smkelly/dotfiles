bcee461bd88fa5ff74bbe1d5f49e6f8fed431bfc
Sean Kelly <smkelly@smkelly.org>
2022-03-12 20:11:48 -0600

    Make paths.fish compatible with older fish versions

--- dot_config/fish/conf.d/paths.fish
+++ dot_config/fish/conf.d/paths.fish
@@ -1,6 +1,16 @@
-functions -q fish_add_path
-  and fish_add_path \
+set -l paths \
   $HOME/bin \
   $HOME/.cargo/bin \
   /opt/homebrew/bin \
   /opt/homebrew/sbin
+
+if functions -q xxfish_add_path
+  fish_add_path $pahs
+else
+  set -l --path checked
+  for dir in $paths
+    test -d $dir; and set -a checked (realpath $dir)
+  end
+  set -p PATH $checked
+end
+
