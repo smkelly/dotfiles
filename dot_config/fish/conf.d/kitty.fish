0c85becd4834efe6f1b8970039995dcd2365078d
Sean Kelly <smkelly@smkelly.org>
2024-02-20 13:16:43 -0500

    Add kssh alias for kitty in fish

--- dot_config/fish/conf.d/kitty.fish
+++ dot_config/fish/conf.d/kitty.fish
@@ -2,5 +2,7 @@ status is-interactive; or exit
 
 test "$TERM" = "xterm-kitty"; or exit
 
-command -qv kitten
-and alias icat 'kitten icat'
+command -qv kitten; or exit
+
+alias icat 'kitten icat'
+alias kssh 'kitten ssh'
