160925c4376e0f0c2a7d58edf0486307b06b4aa6
Sean Kelly <smkelly@smkelly.org>
2024-02-26 09:22:34 -0500

    Update fish-title function to use path at fish prompt

--- dot_config/fish/functions/fish_title.fish
+++ dot_config/fish/functions/fish_title.fish
@@ -5,6 +5,10 @@ function fish_title
   set -q argv[1]
     and set cmd $argv[1]
     or set cmd (status current-command)
+  if test "$cmd" = "fish"
+    set cmd (prompt_pwd)
+  end
+
 
     if test "$SSH_CONNECTION"
       set remote $USER@(string replace -r "\..*" "" $hostname)
