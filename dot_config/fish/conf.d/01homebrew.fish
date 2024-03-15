1b0f6194b28c30393115623405d361174cbfa1e9
Sean Kelly <smkelly@smkelly.org>
2024-03-15 08:08:11 -0400

    Add more homebrew env vars

--- dot_config/fish/conf.d/01homebrew.fish
+++ dot_config/fish/conf.d/01homebrew.fish
@@ -1,5 +1,7 @@
 if test -x /opt/homebrew/bin/brew
   /opt/homebrew/bin/brew shellenv | source
+  set -gx HOMEBREW_AUTOREMOVE 1
+  command -q bat; and set -g HOMEBREW_BAT 1
   set -gx HOMEBREW_NO_ENV_HINTS 1
 end
 
