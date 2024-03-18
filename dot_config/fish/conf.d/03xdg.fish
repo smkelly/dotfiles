ee136d40085efe8c11764f999452649a97e5765c
Sean Kelly <smkelly@smkelly.org>
2024-03-18 13:10:25 -0400

    Add XDG user directory definitions

--- dot_config/fish/conf.d/03xdg.fish
+++ dot_config/fish/conf.d/03xdg.fish
@@ -7,3 +7,12 @@ set -q XDG_STATE_HOME; or \
 set -q XDG_CACHE_HOME; or \
   set -gx --path XDG_CACHE_HOME "$HOME/.cache"
 
+set -gx --path XDG_DESKTOP_DIR    "$HOME/Desktop"
+set -gx --path  XDG_DOCUMENTS_DIR "$HOME/Documents"
+set -gx --path XDG_DOWNLOAD_DIR    "$HOME/Downloads"
+set -gx --path XDG_MUSIC_DIR       "$HOME/Music"
+set -gx --path XDG_PICTURES_DIR    "$HOME/Pictures"
+set -gx --path XDG_PUBLICSHARE_DIR "$HOME/Public"
+set -gx --path XDG_TEMPLATES_DIR   "$HOME/Templates"
+set -gx --path XDG_VIDEOS_DIR      "$HOME/Movies"
+
