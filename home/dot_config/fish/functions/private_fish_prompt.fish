721985bd9f655c95d4111ce3d5bb644d1514af1e
Sean Kelly <smkelly@smkelly.org>
2026-06-29 22:06:31 -0400

    Update fish prompt

--- home/dot_config/fish/functions/private_fish_prompt.fish
+++ home/dot_config/fish/functions/private_fish_prompt.fish
@@ -2,13 +2,15 @@ function fish_prompt --description 'Fast prompt without Starship'
     set -l last_status $status
 
     set -l normal (set_color normal)
-    set -l user_color (set_color yellow)
-    set -l host_color (set_color magenta)
+    set -l user_color (set_color --bold yellow)
+    set -l root_user_color (set_color --bold red)
+    set -l host_color (set_color --bold --dim green)
     set -l sep_color (set_color brblack)
-    set -l cwd_color (set_color cyan)
-    set -l git_color (set_color green)
-    set -l status_color (set_color red)
-    set -l prompt_color (set_color green)
+    set -l cwd_color (set_color --bold cyan)
+    set -l git_color (set_color --bold purple)
+    set -l dirty_color (set_color --bold yellow)
+    set -l status_color (set_color --bold red)
+    set -l prompt_color (set_color --bold green)
 
     set -l is_remote 0
     if set -q SSH_TTY; or set -q SSH_CONNECTION
@@ -23,6 +25,10 @@ function fish_prompt --description 'Fast prompt without Starship'
     set -l show_user 0
     if test -n "$USER"; and test "$USER" != "$login_user"
         set show_user 1
+    end
+    if test "$USER" = root
+        set show_user 1
+        set user_color $root_user_color
     end
 
     if test $is_remote -eq 1; and not set -q __fish_prompt_short_hostname
@@ -41,16 +47,27 @@ function fish_prompt --description 'Fast prompt without Starship'
 
     echo -n -s $cwd_color (prompt_pwd -d 1) $normal
 
-    set -l branch (command git symbolic-ref --quiet --short HEAD 2>/dev/null)
-    if test -z "$branch"
-        set branch (command git rev-parse --short HEAD 2>/dev/null)
-    end
-    if test -n "$branch"
-        echo -n -s ' ' $sep_color 'on ' $git_color ' ' $branch $normal
+    set -l git_status (command git status --porcelain=v1 --branch --ignore-submodules=dirty 2>/dev/null)
+    if test (count $git_status) -gt 0
+        set -l branch (string replace -r '^## ' '' -- $git_status[1])
+        set branch (string replace -r '\.\.\..*$' '' -- $branch)
+        if string match -q 'HEAD*' -- $branch
+            set -l head (command git rev-parse --short HEAD 2>/dev/null)
+            if test -n "$head"
+                set branch @$head
+            end
+        end
+
+        set -l dirty_marker
+        if test (count $git_status) -gt 1
+            set dirty_marker ' *'
+        end
+        echo -n -s ' ' $sep_color 'on ' $git_color ' ' $branch $dirty_color $dirty_marker $normal
     end
 
     if test $last_status -ne 0
         echo -n -s ' ' $status_color '[' $last_status ']' $normal
+        set prompt_color $status_color
     end
 
     echo -n -s ' ' $prompt_color '❯ ' $normal
